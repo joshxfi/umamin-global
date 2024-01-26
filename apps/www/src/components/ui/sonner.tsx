8bddd1c136206a1bdf1401ff01ffbe61725f5624
Dale B <daleban.dev@gmail.com>
2024-01-26 15:39:41 +0800

    chore: warn next-themes error

--- apps/www/src/components/ui/sonner.tsx
+++ apps/www/src/components/ui/sonner.tsx
@@ -1,14 +1,17 @@
-import { useTheme } from "next-themes"
-import { Toaster as Sonner } from "sonner"
+// import { useTheme } from "next-themes";
+import { Toaster as Sonner } from "sonner";
 
-type ToasterProps = React.ComponentProps<typeof Sonner>
+type ToasterProps = React.ComponentProps<typeof Sonner>;
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  /**
+   * TODO: Fix this, next-themes causing error
+   */
+  // const { theme = "system" } = useTheme();
 
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      // theme={theme as ToasterProps["theme"]}
       className="toaster group"
       toastOptions={{
         classNames: {
@@ -23,7 +26,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }}
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
