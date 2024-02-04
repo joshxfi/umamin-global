724789c278e9512682461104cc2be83c2b60dc1f
Josh Daniel <joshxfi.dev@gmail.com>
2024-02-04 15:36:36 +0800

    chore: update info page

--- apps/www/src/app/info/page.tsx
+++ apps/www/src/app/info/page.tsx
@@ -4,28 +4,20 @@ import Markdown from "react-markdown";
 export default function Info() {
   const markdown = `
 # Table of Contents
-1. [What is TheForum?](#about)
-2. [Creating an Account](#authentication)
-3. [Anonymity & Security](#security)
-3. [Content Moderation](#moderation)
-
-## What is TheForum?
-
-If you are here, hello 👋
-
-TheForum is a place for the unheard voices; a place that gives you a voice. A place to share stories, rants, confessions, and more. 
+1. [Creating an Account](#authentication)
+2. [Anonymity & Security](#security)
 
 ## Creating an Account
 
-Browsing content within TheForum doesn't require you to authenticate, however, creating an account allows TheForum to be interactive where you can upvote and comment on posts. To create an account, tap on the user icon from the menu below or go to [/register](https://theforum.vercel.app/register).
+Browsing content within Umamin Global doesn't require you to authenticate, however, creating an account allows Umamin Global to be interactive where you can upvote and comment on posts. 
+
+To create an account, tap on the user icon from the menu below or go to [/login](https://global.umamin.link/login).
 
 ## Anonymity & Security
 
-To ensure anonimity, make sure that your username does not link to your identity. TheForum's authentication process only requires you a username and a password (hashed using *bcrypt*) since emails might contain the user's identity. If you lose your password, you need to create a new account. 
+To ensure anonimity, make sure that your username does not link to your identity.
 
-## Content Moderation
-
-TheForum is an open space, an open space that is safe. Any content that goes against TheForum's [terms of service](https://theforum.vercel.app/terms) will be removed.
+Any content that goes against Umamin Global's [terms of service](https://global.umamin.link/terms) will be removed.
 `;
 
   return (
