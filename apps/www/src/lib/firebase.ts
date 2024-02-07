370dd67b625566849a058bcfad1d82412e7fdc11
Josh Daniel <joshxfi.dev@gmail.com>
2024-02-07 16:04:16 +0800

    chore: update firebase credentials

--- apps/www/src/lib/firebase.ts
+++ apps/www/src/lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC3C9scwkR2tbJHxSodUq46dnyPirUULBQ",
-  authDomain: "umamin-global.firebaseapp.com",
-  projectId: "umamin-global",
-  storageBucket: "umamin-global.appspot.com",
-  messagingSenderId: "355086340864",
-  appId: "1:355086340864:web:ca9653f1f2c7115a80d9e5",
-  measurementId: "G-0S0X0VLGVJ",
+  apiKey: "AIzaSyCFDyEq6l8Zd-WlC6ZzHCUC8Slnxe_SXfY",
+  authDomain: "umamin-app.firebaseapp.com",
+  projectId: "umamin-app",
+  storageBucket: "umamin-app.appspot.com",
+  messagingSenderId: "496522144815",
+  appId: "1:496522144815:web:5cc62492d4b2026d8e2ada",
+  measurementId: "G-KD9PJ5MKHJ",
 };
 
 const app = initializeApp(firebaseConfig);
