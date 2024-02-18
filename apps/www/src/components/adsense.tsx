c7773fa51ffe9aa6b07c4319d4f7467696ae3d9f
Josh Daniel <joshxfi.dev@gmail.com>
2024-02-18 17:17:31 +0800

    feat: add in-feed ads

--- apps/www/src/components/adsense.tsx
+++ apps/www/src/components/adsense.tsx
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
 interface Props {
   slotId: string;
   className?: string;
+  type?: "display" | "in-feed";
 }
 
-const AdSense = ({ slotId, className }: Props) => {
+const AdSense = ({ slotId, className, type = "display" }: Props) => {
   useEffect(() => {
-    if (process.env.NODE_ENV === 'production') {
-      if (typeof window !== 'undefined') {
+    if (process.env.NODE_ENV === "production") {
+      if (typeof window !== "undefined") {
         (window.adsbygoogle = window.adsbygoogle || []).push({});
       }
     }
@@ -16,13 +17,23 @@ const AdSense = ({ slotId, className }: Props) => {
 
   return (
     <div className={className}>
-      <ins
-        className='adsbygoogle block'
-        data-ad-client='ca-pub-4274133898976040'
-        data-ad-slot={slotId}
-        data-ad-format='auto'
-        data-full-width-responsive='true'
-      />
+      {type === "in-feed" ? (
+        <ins
+          className="adsbygoogle block"
+          data-ad-client="ca-pub-4274133898976040"
+          data-ad-slot={slotId}
+          data-ad-format="fluid"
+          data-ad-layout-key="-fb+5w+4e-db+86"
+        />
+      ) : (
+        <ins
+          className="adsbygoogle block"
+          data-ad-client="ca-pub-4274133898976040"
+          data-ad-slot={slotId}
+          data-ad-format="auto"
+          data-full-width-responsive="true"
+        />
+      )}
     </div>
   );
 };
