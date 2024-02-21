e15ad443605fba96b66f3ecd05890218389390f8
Josh Daniel <joshxfi.dev@gmail.com>
2024-02-21 22:25:19 +0800

    chore: add custom output for prisma

--- apps/www/src/lib/db.ts
+++ apps/www/src/lib/db.ts
@@ -1,18 +1,15 @@
-// @ts-nocheck
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@umamin-global/prisma";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
 };
 
-type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
+}
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClientSingleton | undefined;
-};
-
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+const prisma = globalThis.prisma ?? prismaClientSingleton();
 
 export default prisma;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
