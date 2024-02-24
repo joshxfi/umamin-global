baa0d2fcca78b6a7931b578b66c49cca1a46d326
Josh Daniel <joshxfi.dev@gmail.com>
2024-02-24 11:27:35 +0800

    chore: update adsense component

--- apps/www/src/components/adsense.tsx
+++ apps/www/src/components/adsense.tsx
@@ -5,40 +5,26 @@ interface Props {
   slotId: string;
   className?: string;
   test?: boolean;
-  type?: "display" | "in-feed";
 }
 
-const AdSense = ({ slotId, className, test, type = "display" }: Props) => {
+const AdSense = ({ slotId, className, test }: Props) => {
   useEffect(() => {
-    if (process.env.NODE_ENV === "production") {
+    if (process.env.NODE_ENV === "production" && typeof window === "object") {
       (window.adsbygoogle = window.adsbygoogle || []).push({});
     }
   }, []);
 
   return (
-    <div
+    <ins
       className={cn("adsbygoogle", className, {
         "h-24 bg-blue-200": test,
       })}
-    >
-      {type === "in-feed" ? (
-        <ins
-          style={{ display: "block" }}
-          data-ad-client="ca-pub-4274133898976040"
-          data-ad-slot={slotId}
-          data-ad-format="fluid"
-          data-ad-layout-key="-fb+5w+4e-db+86"
-        />
-      ) : (
-        <ins
-          style={{ display: "block" }}
-          data-ad-client="ca-pub-4274133898976040"
-          data-ad-slot={slotId}
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        />
-      )}
-    </div>
+      style={{ display: "block" }}
+      data-ad-client="ca-pub-4274133898976040"
+      data-ad-slot={slotId}
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    />
   );
 };
 
