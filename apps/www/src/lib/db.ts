fa0398b82d2008dc97000fb52bf11c8e2ffd1877
Josh Daniel <joshxfi.dev@gmail.com>
2024-02-27 15:05:38 +0800

    chore(temp): update prisma path

--- apps/www/src/lib/db.ts
+++ apps/www/src/lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@umamin-global/db";
 
 const prismaClientSingleton = () => {
   return new PrismaClient();
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,1 +1,5 @@
+import "reflect-metadata"
+
+// @ts-ignore
+export { PrismaClient } from "./generated/client";
 export * from "./generated/type-graphql";
