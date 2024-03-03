83496609c2fff818dd121c04bd7929d8f8087da0
Josh Daniel <joshxfi.dev@gmail.com>
2024-03-03 19:23:59 +0800

    chore: update cors headers

--- apps/www/next.config.js
+++ apps/www/next.config.js
@@ -1,42 +1,42 @@
 /** @type {import('next').NextConfig} */
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin')
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 const nextConfig = {
-    transpilePackages: [],
-    compiler: {
-        removeConsole: process.env.NODE_ENV === "production",
-    },
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            config.plugins = [...config.plugins, new PrismaPlugin()]
-        }
+  transpilePackages: [],
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 
-        return config
-    },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
 
-    async headers() {
-        return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    {
-                        key: "Access-Control-Allow-Origin",
-                        value: process.env.NEXTAUTH_URL ?? "*",
-                    },
-                    {
-                        key: "Access-Control-Allow-Methods",
-                        value: "GET,DELETE,PATCH,POST,PUT",
-                    },
-                    {
-                        key: "Access-Control-Allow-Headers",
-                        value:
-                            "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-                    },
-                ],
-            },
-        ];
-    },
+    return config;
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXTAUTH_URL,
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
