ba4dd75ececa4c2185d483070945d7ecaa582fd8
Josh Daniel <joshxfi.dev@gmail.com>
2024-03-03 13:23:48 +0800

    feat: implement rate limit with kv

---
+++ apps/www/src/app/blocked/page.tsx
@@ -0,0 +1,23 @@
+import remarkGfm from "remark-gfm";
+import Markdown from "react-markdown";
+
+export default function Info() {
+  const markdown = `
+## Why Did This Happen? 
+Our system has detected that you've made too many requests within a short period. This is likely due to reaching the limit set for accessing our services.
+
+## What Can You Do?
+- **Wait a Moment**: Take a breather and wait for a short while before trying your request again.
+- **Check Your Usage**: Consider optimizing your usage patterns to avoid hitting the rate limit in the future.
+- **Contact Support**: If you believe this message is a mistake or if you need urgent assistance, don't hesitate to reach out to our support team for help.
+
+We appreciate your understanding and cooperation in maintaining the quality and reliability of our services.
+
+`;
+
+  return (
+    <section className="prose prose-invert pb-52 container text-sm">
+      <Markdown remarkPlugins={[remarkGfm]}>{markdown}</Markdown>
+    </section>
+  );
+}
